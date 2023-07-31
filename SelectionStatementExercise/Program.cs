ed1f067f0d9dc2cf1430576ef9025240fa76dfd8
RafaelAdrianMejia <tucsonrafa@outlook.com>
2023-07-30 19:16:13 -0700

    exercise completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            int favNum = r.Next(1, 3);
+            int response = 0;
+
+            while (response != favNum)
+            {
+                Console.Write("Give me a number: ");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNum)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {response}");
+                }
+                else if (response > favNum)
+                {
+                    Console.WriteLine($"Too high!\nYour guess: {response}");
+                }
+                else
+                {
+                    Console.WriteLine($"Correct! {response} was my favorite number.");
+                }
+            }
+
+
         }
     }
 }
